9c01ea18a75739d5c4849158028a31abcb48b712
Shahed <shahed011@hotmail.com>
2017-02-08 02:20:00 +0000

    Fixed currency to gbp

---
+++ Ranaitfleur/Helper/CulturesHelper.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+namespace Ranaitfleur.Helper
+{
+    public static class CulturesHelper
+    {
+        public static readonly CultureInfo UnitedKingdom = new CultureInfo("en-Gb");
+    }
+}
