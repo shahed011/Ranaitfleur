b3d692031a39ffe71a950af8930d8b2b6f79f0fe
Shahed <shahed011@hotmail.com>
2017-02-15 01:53:06 +0000

    A bit of new code on email system. Somehow fixed the product page.

--- Ranaitfleur/Services/MailService.cs
+++ Ranaitfleur/Services/MailService.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics;
-using System.Net;
 using Microsoft.Extensions.Logging;
 using MimeKit;
 using MailKit.Net.Smtp;
@@ -20,31 +19,39 @@ namespace Ranaitfleur.Services
             Debug.WriteLine($"Sending Mail: To:{to} From:{from} Subject:{subject}");
         }
 
-        public async void SendMail(string to, string from, string subject, string body)
+        public void SendMail(string to, string from, string subject, string body)
         {
             var emailMessage = new MimeMessage();
 
-            emailMessage.From.Add(new MailboxAddress("Ranaitfleur", from));
-            emailMessage.To.Add(new MailboxAddress("Ranaitfleur", to));
+            emailMessage.From.Add(new MailboxAddress("Ranaitfleur", "postmaster@ranaitfleur.com"));
+            emailMessage.To.Add(new MailboxAddress("Ranaitfleur", "info@ranaitfleur.com"));
             emailMessage.Subject = subject;
             emailMessage.Body = new TextPart("plain") { Text = body };
 
             using (var client = new SmtpClient())
             {
-                var credentials = new NetworkCredential
-                {
-                    UserName = "YourEMAIL", // replace with valid value
-                    Password = "YourPASSWORD" // replace with valid value
-                };
-                
-                // for accepting every ssl certificate, DO NOT USE ON PROD, used for testing with gmail
                 client.ServerCertificateValidationCallback = (s, certificate, chain, sslPolicyErrors) => true;
+                client.Connect("mail.ranaitfleur.com", 465, true);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                client.Authenticate("postmaster@ranaitfleur.com", "#Gunners#69#");
 
-                // check your smtp server setting and amend accordingly:
-                await client.ConnectAsync("smtp.gmail.com", 465, true).ConfigureAwait(false);
-                await client.AuthenticateAsync(credentials);
-                await client.SendAsync(emailMessage).ConfigureAwait(false);
-                await client.DisconnectAsync(true).ConfigureAwait(false);
+                client.Send(emailMessage);
+                client.Disconnect(true);
+                //var credentials = new NetworkCredential
+                //{
+                //    UserName = "postmaster@ranaitfleur.com", // replace with valid value
+                //    Password = "#Gunners#69#", // replace with valid value
+                //    Domain = "mail.ranaitfleur.com"
+                //};
+
+                //// for accepting every ssl certificate, DO NOT USE ON PROD, used for testing with gmail
+                //client.ServerCertificateValidationCallback = (s, certificate, chain, sslPolicyErrors) => true;
+
+                //// check your smtp server setting and amend accordingly:
+                //await client.ConnectAsync("mail.ranaitfleur.com", 465, true).ConfigureAwait(false);
+                //await client.AuthenticateAsync(credentials);
+                //await client.SendAsync(emailMessage).ConfigureAwait(false);
+                //await client.DisconnectAsync(true).ConfigureAwait(false);
             }
         }
     }
